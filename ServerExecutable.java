c6e6a8c0d49d97e78305d24dadd9dbc5360822a9
Koval0k <penguin10456@gmail.com>
2025-09-05 08:43:39 +0300

    Delete ServerExecutable.java

--- ServerExecutable.java
+++
@@ -1,9 +0,0 @@
-package org.example.server.commands;
-
-import java.io.IOException;
-import org.example.common.dto.*;
-
-public interface ServerExecutable {
-    public Response execute(Request request) throws IOException;
-
-}
